d99ffb48527447933b1887f81e5c60e203829eb7
AdamAlibraheem000 <adamalibraheem@hotmail.com>
2022-06-20 17:08:57 -0400

    Added react router & links

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 const App = () => 
+<Router>
 <Fragment>
   <Navbar></Navbar>
-  <Landing></Landing>
+  <Routes>
+    <Route path="/" element={<Landing/>}></Route>
+      <Route className="container" path="/login" element={<Login/>}></Route>
+      <Route className="container" path="/register" element={<Register/>}></Route>
+  </Routes>
 </Fragment>
+</Router>
   
 
 export default App;

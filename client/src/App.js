1847d9573fe12e13010c8fba4c2fe22d94824e1d
AdamAlibraheem <adamalibraheem@hotmail.com>
2022-06-26 11:31:47 -0400

    updated app to react-router-dom v6

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import  Login  from './components/auth/Login';
+import  Register  from './components/auth/Register';
 import './App.css';
 
-const App = () => 
+const App = () => (
+<Router>
 <Fragment>
   <Navbar></Navbar>
-  <Landing></Landing>
+  <Routes>
+  <Route  path='/' element={<Landing/>}></Route>
+  </Routes>
+  <section className='container'>
+    <Routes>
+      <Route  path="/register" element={<Register/>} ></Route>
+      <Route  path="/login" element={<Login/>} ></Route>
+    </Routes>
+  </section>
 </Fragment>
-  
+</Router>
+);
 
 export default App;
